41a3d321a58f5861f12849ed57ca2121ce00ca20
ayyluhhh <156031394+ayyluhhh@users.noreply.github.com>
2024-01-08 17:43:01 -0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+
+        int num1 = 2;
+        int num2 = 4;
+        int sum = 0;
+
+        sum = num1 + num2;
+
+        System.out.println("\n\n The sum of " + num1 + " and " + num2 + " is: " + sum + "\n\n" );
+    }
+}
